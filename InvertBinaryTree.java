ba92ff38755cf8d7936c21dea6da894cbaaf91e4
Joyesh Kakkar <joyeshkakkar@gmail.com>
2015-11-14 06:29:16 -0500

    Invert Binary Tree

---
+++ InvertBinaryTree.java
@@ -0,0 +1,35 @@
+import SymmetricTree.TreeNode;
+
+/*Invert a binary tree.
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+to
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1*/
+public class InvertBinaryTree {
+	/**
+	 * Definition for a binary tree node.
+	 */
+	private class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+		TreeNode(int x) { val = x; }
+	}
+
+	public TreeNode invertTree(TreeNode root) {
+		if((root==null) || (root.left==null && root.right==null))
+			return root;
+		TreeNode temp= root.left;
+		root.left=invertTree(root.right);
+		root.right=invertTree(temp);
+		return root;
+	}
+}
